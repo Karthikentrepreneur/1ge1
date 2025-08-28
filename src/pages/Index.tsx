7e87c0b83e06f962d2b853aed6f51e2a079aa976
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-08-28 17:59:19 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
 import GroupCompanies from "@/components/GroupCompanies";
 import Footer from "@/components/Footer";
-import ServicesAndSolution from "@/components/ServicesAndSolution";
+import { ServicesAndSolutions } from "@/components/ServicesAndSolutions";
 const Index = () => {
   return (
     <div className="min-h-screen">
